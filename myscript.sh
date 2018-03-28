343c0516077000e5aa052be3d7a5267a1751b88e
annambharadwaj <annambharadwaj@gmail.com>
2018-03-28 17:09:15 -0400

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "Testing"
-echo "Argument one is $1"
-echo "Argument two is $2"
-echo "Argument three is $3"
-sum=$(($1 + $2 + $3))
+echo "Argument one is 1"
+echo "Argument two is 2"
+echo "Argument three is 3"
+sum=$((1 + 2 + 3))
 echo "Total is $sum"
 
